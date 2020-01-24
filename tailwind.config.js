93e5119c8460fa50b0d2a3770d9cbf0439fc4390
Kyle Plaugher <kaplaugher94@gmail.com>
2020-01-24 15:24:03 -0500

    updates

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        'gray-750': '#3f495a',
+        'gray-850': '#222733',
+        'gray-900-spotify': '#121212',
+        'gray-800-spotify': '#181818',
+        'gray-700-spotify': '#282828'
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+};
